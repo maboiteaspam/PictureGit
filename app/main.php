ee14886b41c4cb2dfe870fab378ed59cda2b7f65
clement <maboiteaspam@wanadoo.fr>
2014-04-01 21:55:54 +0800

    code indentation

--- app/main.php
+++ app/main.php
@@ -56,28 +56,28 @@ if( $VS->isRootReady() === false ){
 
 $routes = array();
 $routes["`^/list_directory/(.*)`i"] = function($path) use($picture_dir){
-	$path = urldecode( $path );
-	$retour = read_directory($picture_dir.$path);
-	$retour = relative_to($path, $retour);
-	return respond_json($retour);
+    $path = urldecode( $path );
+    $retour = read_directory($picture_dir.$path);
+    $retour = relative_to($path, $retour);
+    return respond_json($retour);
 };
 $routes["`^/list_directories/(.*)`i"] = function($path) use($picture_dir){
-	$path = urldecode( $path );
-	$retour = read_directory($picture_dir.$path);
-	$retour = filter_dirs($picture_dir.$path, $retour);
-	$retour = relative_to($path, $retour);
-	return respond_json($retour);
+    $path = urldecode( $path );
+    $retour = read_directory($picture_dir.$path);
+    $retour = filter_dirs($picture_dir.$path, $retour);
+    $retour = relative_to($path, $retour);
+    return respond_json($retour);
 };
 $routes["`^/list_files/(.*)`i"] = function($path) use($picture_dir){
-	$path = urldecode( $path );
-	$retour = read_directory($picture_dir.$path);
-	$retour = filter_files($picture_dir.$path, $retour);
-	$retour = relative_to($path, $retour);
-	return respond_json($retour);
+    $path = urldecode( $path );
+    $retour = read_directory($picture_dir.$path);
+    $retour = filter_files($picture_dir.$path, $retour);
+    $retour = relative_to($path, $retour);
+    return respond_json($retour);
 };
 $routes["`^/read_file/(.+)`i"] = function($path) use($picture_dir){
-	$path = urldecode( $path );
-	return respond_file($picture_dir.$path);
+    $path = urldecode( $path );
+    return respond_file($picture_dir.$path);
 };
 $routes["`^/read_file/([^@]+)@(.+)`i"] = function($f_path, $version) use($picture_dir){
 };
@@ -94,19 +94,19 @@ $routes["`^/trash_file/(.+)`i"] = function($path) use($picture_dir, $VS){
 $routes["`^/read_logs/(.+)`i"] = function($f_path) use($picture_dir){
 };
 $routes["`^/list_bootstrap_themes`i"] = function() use($assets_dir,$www_dir){
-	$retour = read_directory($assets_dir."/themes/");
-	$retour = filter_pattern("`[^.]+[.]bootstrap[.]min[.]css$`", $retour);
-	$retour = relative_to("/assets/themes/", $retour);
-	return respond_json($retour);
+    $retour = read_directory($assets_dir."/themes/");
+    $retour = filter_pattern("`[^.]+[.]bootstrap[.]min[.]css$`", $retour);
+    $retour = relative_to("/assets/themes/", $retour);
+    return respond_json($retour);
 };
 $routes["`^/config$`"] = function() use($config){
     return respond_json($config);
 };
 $routes["catch_all"] = function($path) use($www_dir){
-	if( is_file($www_dir.$path) ){
-		return respond_file($www_dir.$path);
-	}
-	return false;
+    if( is_file($www_dir.$path) ){
+        return respond_file($www_dir.$path);
+    }
+    return false;
 };
 
 $request_uri = isset($_SERVER["REQUEST_URI"])?$_SERVER["REQUEST_URI"]:"";
@@ -114,19 +114,19 @@ $q_str = isset($_SERVER["QUERY_STRING"])?$_SERVER["QUERY_STRING"]:"";
 $request_uri = str_replace("?".$q_str,"",$request_uri);
 $found = false;
 foreach( $routes as $pattern => $handler ){
-	if( $pattern != "catch_all" ){
-		$matches = array();
-		if( preg_match($pattern, $request_uri, $matches) ) {
-			array_shift($matches);
-			echo call_user_func_array($handler,$matches);
-			$found = true;
-			break;
-		}
-	}
+    if( $pattern != "catch_all" ){
+        $matches = array();
+        if( preg_match($pattern, $request_uri, $matches) ) {
+            array_shift($matches);
+            echo call_user_func_array($handler,$matches);
+            $found = true;
+            break;
+        }
+    }
 }
 // router.php
 if (!$found && isset($routes["catch_all"])) {
-		echo call_user_func_array($routes["catch_all"],array($request_uri));
-		$found = true;
+    echo call_user_func_array($routes["catch_all"],array($request_uri));
+    $found = true;
 }
 return $found;
