50dee85429e535e9159a1fbc71114712804196d5
clement <maboiteaspam@wanadoo.fr>
2014-04-13 21:23:11 +0800

    implement pager support

--- app/www2/assets/lib/PictureGitApi.js
+++ app/www2/assets/lib/PictureGitApi.js
@@ -7,14 +7,23 @@ define([],function(){
     that.fetchConfig = function(){
       return ajaxHelper.getJSON("/config");
     };
-    that.fetchDirectoryItems = function(path){
-      return ajaxHelper.getJSON("/list_directory/"+path);
+    that.fetchDirectoryItems = function(path,from,items_by_page){
+      var url = "/list_directory/"+path+"?";
+      if( from ) url+= "from="+from+"&";
+      if( items_by_page ) url+= "items_by_page="+items_by_page+"&";
+      return ajaxHelper.getJSON(url);
     };
-    that.fetchDirectories = function(path){
-      return ajaxHelper.getJSON("/list_directories/"+path);
+    that.fetchDirectories = function(path,from,items_by_page){
+      var url = "/list_directories/"+path+"?";
+      if( from ) url+= "from="+from+"&";
+      if( items_by_page ) url+= "items_by_page="+items_by_page+"&";
+      return ajaxHelper.getJSON(url);
     };
-    that.fetchPictures = function(path){
-      return ajaxHelper.getJSON("/list_files/"+path);
+    that.fetchPictures = function(path,from,items_by_page){
+      var url = "/list_files/"+path+"?";
+      if( from ) url+= "from="+from+"&";
+      if( items_by_page ) url+= "items_by_page="+items_by_page+"&";
+      return ajaxHelper.getJSON(url);
     };
     that.trashFile = function(path){
       return ajaxHelper.getJSON("/trash_file/"+path);
