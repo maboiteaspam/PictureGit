2a336936193fbba4e849068d7768677e8c676ec7
clement <maboiteaspam@wanadoo.fr>
2014-04-21 20:50:52 +0800

    add pre filters for data and error to dataresource

--- app/www/assets/lib/DataResource.js
+++ app/www/assets/lib/DataResource.js
@@ -7,12 +7,21 @@ define([],function(){
     that.response = ko.observable(null);
     that.errors = ko.observable(null);
 
+    that.data_filters = [];
+    that.error_filters = [];
+
     that.update = function(handler){
       that.loaded(false);
       return handler.fail(function(err){
+        for(var n in that.error_filters ){
+          that.error_filters[n](err);
+        }
         that.errors(err);
         that.response(null);
       }).done(function(data){
+        for(var n in that.data_filters ){
+          that.data_filters[n](data);
+        }
         that.errors(null);
         that.response(data);
       }).always(function(){
